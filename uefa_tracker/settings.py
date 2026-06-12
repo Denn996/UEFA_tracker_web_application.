4e5ae05c542357fcc3696075b3828a08fe5f5f63
Denn996 <dennisgitau831@gmail.com>
2026-06-12 10:27:29 +0300

    Update change

--- uefa_tracker/settings.py
+++ uefa_tracker/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'uefa_tracker.urls'
 
+TEMPLATES = BASE_DIR / 'templates'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
